c790318d6eaf3dd3a51f2d3973922dab74f022a0
mrunmoyi <naikmrunmayi03@gmail.com>
2024-11-07 13:55:37 +0530

    updated home , navbar, contactme

--- src/components/Pages/Navbar.js
+++ src/components/Pages/Navbar.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import './Navbar.css'; // Import your CSS file
 import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 import { IoMdMailOpen } from "react-icons/io";
-
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,16 +13,23 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-    <div className="icons"> 
-      <div className="github"><FaGithub/></div>&nbsp;&nbsp;&nbsp;&nbsp;
-      <div className="linkedin"><FaLinkedin/></div>&nbsp;&nbsp;&nbsp;&nbsp;
-      <div className="mail"><IoMdMailOpen/></div>
-    </div>
-     
+      <div className="icons">
+        <div className="github">
+          <FaGithub />
+        </div>
+        <div className="linkedin">
+          <FaLinkedin />
+        </div>
+        <div className="mail">
+          <IoMdMailOpen />
+        </div>
+      </div>
+
       <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
         <a href="#projects">Projects</a>
-        <a href="#Contactme">Contact Me</a>
+        <a href="#contactme">Contact Me</a>
       </div>
+      
       <div className="navbar-toggle" onClick={toggleNavbar}>
         ☰
       </div>
